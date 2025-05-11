da9f23487c979dc4c5a63a34e958858024fec101
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-05-11 22:02:52 +0200

    Add ExtractDomainFromEmail action

---
+++ src/Domain/Email/Actions/ExtractDomainFromEmail.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Backstage\Laravel\Users\Domain\Email\Actions;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ExtractDomainFromEmail
+{
+    use AsAction;
+
+    public function handle(string $email): string
+    {
+        return $this->extractDomain($email);
+    }
+
+    protected function extractDomain(string $email): string
+    {
+        return substr(strrchr($email, "@"), 1);
+    }
+}
