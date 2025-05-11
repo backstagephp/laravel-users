e4dfbaf91f605946b9d1e9ea2d7c807aada0530a
Manoj Hortulanus <manoj@vormkracht10.nl>
2025-05-11 21:49:46 +0200

    Add base validate email action

--- helpers.php
+++ helpers.php
@@ -4,7 +4,7 @@ if (! function_exists('geo')) {
     function geo($attribute = '')
     {
         if (! session('geo')) {
-            $geo = json_decode(@file_get_contents('https://pro.ip-api.com/json/'.request()->ip().'?key='.config('services.ip-api.key')));
+            $geo = json_decode(@file_get_contents('https://pro.ip-api.com/json/' . request()->ip() . '?key=' . config('services.ip-api.key')));
 
             session()->put('geo', $geo);
         } else {
@@ -12,5 +12,19 @@ if (! function_exists('geo')) {
         }
 
         return $attribute && isset($geo->{$attribute}) ? $geo->{$attribute} : null;
+    }
+}
+
+if (!function_exists('generate_password')) {
+    function generate_password(...$args)
+    {
+        return \Backstage\Laravel\Users\Domain\Password\Actions\GeneratePassword::run(...$args);
+    }
+}
+
+if (!function_exists('validate_email')) {
+    function validate_email(...$args)
+    {
+        return \Backstage\Laravel\Users\Domain\Email\Actions\ValidateEmail::run(...$args);
     }
 }
---
+++ src/Domain/Email/Actions/ValidateEmail.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Backstage\Laravel\Users\Domain\Email\Actions;
+
+use Lorisleiva\Actions\Concerns\AsAction;
+
+class ValidateEmail
+{
+    use AsAction;
+
+    public function handle(string|array $email): bool|array
+    {
+        if (is_array($email)) {
+            if (count($email) == 1) {
+                $email = $email[0];
+            } elseif (count($email) > 1) {
+                return collect($email)->mapWithKeys(fn($e) => [$e => $this->validateSingleEmail($e)])->toArray();
+            }
+        }
+
+        return $this->validateSingleEmail($email);
+    }
+
+    protected function validateSingleEmail(string $email): bool
+    {
+        if (empty($email)) {
+            return false;
+        }
+
+        if (! $this->validateEmail($email)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function validateEmail(string $email): bool
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false && $this->checkDnsrr($email);
+    }
+
+    protected function checkDnsrr(string $email): bool
+    {
+        $domain = substr(strrchr($email, "@"), 1);
+        return checkdnsrr($domain, "MX");
+    }
+}
