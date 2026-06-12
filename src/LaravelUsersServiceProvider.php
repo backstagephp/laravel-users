567d22e88a85876e2ad85cf8428590b1ffe6dd96
markvaneijk <1925388+markvaneijk@users.noreply.github.com>
2026-06-12 14:23:16 +0000

    Fix styling

--- src/LaravelUsersServiceProvider.php
+++ src/LaravelUsersServiceProvider.php
@@ -39,7 +39,7 @@ class LaravelUsersServiceProvider extends PackageServiceProvider
 
     protected function getMigrations(): array
     {
-        $migrationPath = __DIR__ . '/../database/migrations/';
+        $migrationPath = __DIR__.'/../database/migrations/';
 
         $files = File::allFiles($migrationPath);
 
